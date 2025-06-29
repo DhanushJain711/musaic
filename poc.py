ad3717baf1b883fe22161cc3665b51bc0b4dae07
Aarash Zakeri <izear21@gmail.com>
2025-06-28 20:28:24 -0500

    added comments to class + misc main stuff

--- poc.py
+++ poc.py
@@ -2,7 +2,12 @@ import madmom
 
 class SpectralFlux():
     def __init__(self, wav_file):
-        spec = madmom.audio.spectrogram.Spectrogram(wav_file,frame_size=2048,hop_size=200,fft_size=4096)
-        self.sf = madmom.features.onsets.spectral_flux(spec)
-        
+        spec = madmom.audio.spectrogram.Spectrogram(wav_file,frame_size=2048,hop_size=200,fft_size=4096) #makes spectrogram
+        self.sf = madmom.features.onsets.spectral_flux(spec) # onset detection
+
+def main():
+    pass #func would probably loop through folder with training data and init spectral flux
+
+if __name__ == "__main__":
+    main()
         
