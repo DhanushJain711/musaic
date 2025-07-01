ff0527ae64c77c52bc508c45878cf21bac161f4a
Aarash Zakeri <izear21@gmail.com>
2025-06-30 20:12:01 -0500

    better class names + documentation + returning onset detection in poc.py

--- poc.py
+++ poc.py
@@ -3,10 +3,13 @@ import madmom
 
 class SpectralFlux:
     def __init__(self, wav_file):
+        """
+        Creates spectrogram, and then runs spectral flux for onset detection
+        """
         spec = madmom.audio.spectrogram.Spectrogram(
             wav_file, frame_size=2048, hop_size=200, fft_size=4096
-        )  # makes spectrogram
-        self.sf = madmom.features.onsets.spectral_flux(spec)  # onset detection
+        )
+        return madmom.features.onsets.spectral_flux(spec)
 
 
 def main():
--- poc_log.py
+++ poc_log.py
@@ -2,13 +2,20 @@ import madmom
 import numpy as np
 
 
-class SuperFluxWithLogFiltering:
+class SuperFlux: #spectral flux but with log filtering instead of linear filtering
     def __init__(self, num_bands=24, diff_max_bins=3, positive_diffs=True):
+        """
+        This init allows us to set optional parameters before doing log filtering
+        """
         self.num_bands = num_bands
         self.diff_max_bins = diff_max_bins
         self.positive_diffs = positive_diffs
+        
 
     def process(self, data):
+        """
+        runs actual superflux, which is log filtered instead of linearly filtered
+        """
         spec = madmom.audio.spectrogram.LogarithmicFilteredSpectrogram(
             data, num_bands=self.num_bands
         )
